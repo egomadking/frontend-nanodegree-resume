fafb36cc36772cdcc2207e90223a4f11deb273fc
Roy Mosby <ego.madking@gmail.com>
2017-08-03 16:53:15 -0400

    course progression

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Roy Mosby");
+
+var formattedRole = HTMLheaderRole.replace("%data%","Information Architect");
+
+$("#main").prepend(formattedRole);
+$("#main").prepend(formattedName);
