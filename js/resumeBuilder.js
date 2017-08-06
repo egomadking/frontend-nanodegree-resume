b8983d14f018262b864b90cc7fb9627decf9503b
egomadking <ego.madking@gmail.com>
2017-08-06 14:30:25 -0400

    Course progress in JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,39 @@ var formattedRole = HTMLheaderRole.replace("%data%","Information Architect");
 
 $("#main").prepend(formattedRole);
 $("#main").prepend(formattedName);
+
+
+
+var skills = ["system planning", "user experience", "content management", "SharePoint administration"];
+
+var bio = {
+    "fullName": "Roy Mosby",
+    "role": "Life long learner",
+    "contactInfo": "roy.e.mosby@gmail.com",
+    "pictureURL": "http://lorempixel.com/g/400/200/food/",
+    "welcomeMessage": "Sound-free for a better internet",
+    "skills": skills
+};
+
+// $("#main").append(bio.fullName);
+// $("#main").append(bio.role);
+// $("#main").append(bio.contactInfo);
+// $("#main").append(bio.pictureURL);
+// $("#main").append(bio.welcomeMessage);
+// $("#main").append(bio.skills);
+
+var work = {
+    "jobPosition": "Content Manager",
+    "employer": "U.S. Army",
+    "yearsWorked": "2001-current",
+    "location": "various"
+};
+var education = {
+    "school": "Southern New Hampshire University",
+    "yearsAttended": "2015-2017",
+    "degree": "Bachelors in IT",
+    "location": "Online"
+};
+
+$("#main").append(education.school);
+$("#main").append(work["employer"]);
