fe521d672d864f05fcaa2470a790c3d69b97638e
Roy Mosby <ego.madking@gmail.com>
2017-08-07 14:49:44 -0400

    added profile objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,45 +1,90 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-var formattedName = HTMLheaderName.replace("%data%", "Roy Mosby");
+var work = {
+    "jobs": [
+        {
+            "employer": "JMSC",
+            "title": "Operations NCO",
+            "location": "Germany",
+            "dates": "2011-2015",
+            "description": [
+                "did this",
+                "did that",
+                "did some more"
+            ]
+        },
+        {
+            "employer": "3ID",
+            "title": "Content Management NCO",
+            "location": "Georgia",
+            "dates": "2015 - present",
+            "description": [
+                "did this",
+                "did that",
+                "did some more"
+            ]
+        }
+    ]
+};
 
-var formattedRole = HTMLheaderRole.replace("%data%","Information Architect");
-
-$("#main").prepend(formattedRole);
-$("#main").prepend(formattedName);
-
-
-
-var skills = ["system planning", "user experience", "content management", "SharePoint administration"];
+var projects = {
+    "projects": [
+        {
+            "name": "portal migration",
+            "dates": "Oct 16 - Jan 17",
+            "description": "Army division level portal migration from SP10 to SP13",
+            "images": ["images/portalLogo.jpg", "images/portalSnapshot.jpg", "images/portalTemplate.jpg", "images/portalSoldier.jpg"]
+        },
+        {
+            "name": "Jolly Humor Ice Cream",
+            "dates": "June - Jul 16",
+            "description": "Static website for a fictitious ice cream company",
+            "images": ["images/JHlogo.jpg", "images/JHsnapshot.jpg"]
+        }
+    ]
+};
 
 var bio = {
-    "fullName": "Roy Mosby",
-    "role": "Life long learner",
-    "contactInfo": "roy.e.mosby@gmail.com",
-    "pictureURL": "http://lorempixel.com/g/400/200/food/",
-    "welcomeMessage": "Sound-free for a better internet",
-    "skills": skills
+    "name": "Roy Mosby",
+    "role": "Content Manager",
+    "welcomeMessage": "Getting design out of the way of content",
+    "biography": "This is a bunch of crap about me",
+    "contacts": {
+        "mobile": "phone numer",
+        "email": "roy.e.mosby@gmail.com",
+        "githib": "egomadking",
+        "twitter": "@royemosby",
+        "location": "Savannah, GA"
+    }
 };
 
-// $("#main").append(bio.fullName);
-// $("#main").append(bio.role);
-// $("#main").append(bio.contactInfo);
-// $("#main").append(bio.pictureURL);
-// $("#main").append(bio.welcomeMessage);
-// $("#main").append(bio.skills);
-
-var work = {
-    "jobPosition": "Content Manager",
-    "employer": "U.S. Army",
-    "yearsWorked": "2001-current",
-    "location": "various"
-};
 var education = {
-    "school": "Southern New Hampshire University",
-    "yearsAttended": "2015-2017",
-    "degree": "Bachelors in IT",
-    "location": "Online"
+    "schools": [
+        {
+            "name": "Southern New Hampshire University",
+            "location": "Online",
+            "dates": "2015-2017",
+            "url": "https://snhu.edu",
+            "majors": ["Bachelors of science in IT"]
+        },
+        {
+            "name": "Art Institute Online",
+            "location": "Online",
+            "dates": "2006-2008",
+            "url": "https://aiu.edu",
+            "majors": ["Studied graphic design"]            
+        }
+    ],
+    "courses": [
+        {
+            "title": "AKMQC",
+            "school": "Combined Arms Center",
+            "dates": "Aug 2014",
+            "url": "cac.mil",
+        },
+        {
+            "title": "Reactive web design",
+            "school": "Udacity",
+            "dates": "Jul 2017",
+            "url": "udacity.com",
+        }
+    ]
 };
-
-$("#main").append(education.school);
-$("#main").append(work["employer"]);
